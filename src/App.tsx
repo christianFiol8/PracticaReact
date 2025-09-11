a10210d8377f3f4861555753a49865d70006cbf7
Christian <fiolc5173@gmail.com>
2025-09-11 10:17:06 -0700

    Cambios en header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,23 @@ function App(){
   return(
     <>
       <Header />
-      <h1>Hello World</h1>
+      <div className="left-content">
+        <h2>Grown your skills, define your future</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          In hendrerit lobortis leo, quis convallis risus aliquet quis. 
+          Duis non congue velit, sit amet volutpat ante. 
+        </p>
+        <button>
+          Explorar cursos
+        </button>
+        <button>
+          Más información
+        </button>
+      </div>
+      <div className=">right-content">
+        <img></img>
+      </div>
     </>
   )
 }
